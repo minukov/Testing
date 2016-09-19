b2f381666554eed59464894aabf0247feb18d138
minukov <banderloges@mail.ru>
2016-09-19 17:28:21 +0300

    Add files via upload

--- validateForm/script.js
+++ validateForm/script.js
@@ -1,53 +1,113 @@
 /**
  * Created by anton on 16.09.16.
  */
+var mails = ['test@mail.ru', 'email@gmail.com'];
+
 function FV() {
     this.init();
 }
 
 FV.prototype.init = function () {
-    //document.getElementById("login").addEventListener("invalid",this.invalidLogin.bind(this));
-    // $("#login").on("invalid", this.invalidLogin.bind(this));
     $("button").on("click", this.validateForm.bind(this));
-}
-
-
-FV.prototype.invalidLogin = function (e) {
-    console.log("e = ", e);
-    var label = $('label[for="' + $(e.target).attr('id') + '"]');
-    label.addClass("control-label");
-    e.target.setCustomValidity('TEST');
+    $("input").on("change", this.validateInput.bind(this));
 }
 
 FV.prototype.validateForm = function (e) {
-    console.log("e = ", e);
-    var inputs = $("input[type='text']");
+    var inputs = $("input[type='text'], input[type='password']");
+    var i = 0, l = inputs.length,
+        val, input, mess, type;
 
-    var i = 0, l = inputs.length, label,
-        type, val, regexp,
-        invalid = false;
     for (; i < l; i++) {
-        label = $('label[for="' + $(inputs[i]).attr('id') + '"]');
-        label.removeClass("control-label");
-        val = $(inputs[i]).val();
-        console.log("val = ", inputs[i]);
-        if ($(inputs[i]).parent().hasClass("required") && !val) {
-            label.addClass("control-label");
-            invalid = true;
+        input = $(inputs[i]);
+        if (!input.parent().hasClass('required')) {
+            continue;
         }
-        type = $(inputs[i]).data("type");
-        console.log("type = ", type);
-        switch (type) {
-            case 'email':
-                break;
-            case  'pass':
-                break;
-            case 'conf':
-                if ($('#pass').val() !== val) {console.log('re');}
-                break;
+        val = input.val();
+        if (!val) {
+            mess = ' * Обязательны для заполнения';
+            this.setValidity(input, mess);
+        } else if (input.hasClass('inv')) {
+            type = input.data("type");
+            mess = this.checkInput(type, val);
+            this.setValidity(input, mess);
         }
     }
+
+    if (!mess) {
+        input = $("input[type='checkbox']");
+        val = input.is(':checked');
+        if (!val) {
+            mess = ' * Надо согласиться';
+            var label = $('label[for="' + input.attr('id') + '"]');
+            label.addClass("control-label");
+        }
+        this.setValidity(input, mess);
+    }
+
+    if (mess) {
+        return false;
+    }
+    console.log('FORM VALID!!!');
     return false;
+}
+
+FV.prototype.validateInput = function (e) {
+    var target = $(e.target),
+        label = $('label[for="' + target.attr('id') + '"]'),
+        val = target.val(),
+        mess = '', type;
+    if (target.parent().hasClass('required') && !val) {
+        label.addClass("control-label");
+        mess = ' * Обязательны для заполнения';
+    } else {
+        label.removeClass("control-label");
+    }
+    if (mess) {
+        this.setValidity(target, mess);
+        return;
+    }
+    type = target.data("type");
+    mess = this.checkInput(type, val);
+    if (mess) {
+        this.setValidity(target, mess);
+        return;
+    }
+    this.setValidity(target);
+}
+
+FV.prototype.checkInput = function (type, value) {
+    var mess, valid;
+    switch (type) {
+        case 'email':
+            valid = (mails.indexOf(value) === -1);
+            mess = 'Адрес эл. почты уже занят';
+            break;
+        case 'pass':
+            valid = /[0-9]/.test(value) && /[A-Za-z]/.test(value) && (value.length > 4);
+            mess = 'Пароль должен содержать цифры и буквы и быть не короче 5 символов';
+            this.validateInput({target: $('input[id="conf"]')});
+            break;
+        case 'conf':
+            valid = (value === $('input[id="pass"]').val());
+            mess = 'Пароль и подтверждение не совпадают';
+            break;
+    }
+    if (valid) {
+        return '';
+    }
+    return mess;
+}
+
+FV.prototype.setValidity = function (input, message) {
+    if (!message) {
+        $("span")[0].innerHTML = '';
+        input.removeClass("inv");
+        $("button").attr("disabled", false);
+        return;
+    }
+    input.addClass("inv");
+    $("button").attr("disabled", true);
+    $("span")[0].innerHTML = message;
 }
 
 new FV();
