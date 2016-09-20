7e1aee1051dc724461b94bbe6fd9b2eff216991d
minukov <banderloges@mail.ru>
2016-09-20 18:32:38 +0300

    Add files via upload

--- slider/script.js
+++ slider/script.js
@@ -3,22 +3,59 @@
  */
 
 
-function Slider(elem, delay) {
+function Slider(elem, options) {
+    this.$elem = $(elem);
+    var options = options || {};
     this.interval = undefined;
     this.startInterval = undefined;
-    this.delay = undefined;
-    this.init(elem, delay);
+    this.maxImgWidth = options.maxImgWidth || '800px';
+    this.selectedIndex = options.selectedIndex || 1;
+    this.slideDelay = options.slideDelay || 2000;
+    this.startDelay = options.startDelay || 5000;
+    this.init(this.$elem, options.imgs || []);
 }
 
-Slider.prototype.init = function (elem, delay) {
-    this.$elem = $(elem);
-    this.delay = delay || 2000;
-    this.selectedIndex = 1;
-    // this.interval = setInterval(this.slide.bind(this, 1), this.delay);
-    this.$elem.find('.gallery').on("click", this.stopSlide.bind(this));
-    this.$elem.find('span').on("click", this.selectSlide.bind(this));
-    this.$elem.find('.right').on("click", this.slide.bind(this, 1));
-    this.$elem.find('.left').on("click", this.slide.bind(this, -1));
+
+Slider.prototype.init = function (element, imageArray) {
+    var $parent, $elem, $img, $span,
+        i = 0, l = imageArray.length;
+    if (l === 0) {
+        return;
+    }
+    $parent = $("<div></div>");
+    $parent.attr("class", "gallery");
+    $parent.css("max-width", this.maxImgWidth);
+    $parent.on("click", this.stopSlide.bind(this));
+
+    $elem = $("<a></a>");
+    $elem.attr("class", "left");
+    $elem.on("click", this.slide.bind(this, -1));
+    $parent.append($elem);
+
+    $elem = $("<a></a>");
+    $elem.attr("class", "right");
+    $elem.on("click", this.slide.bind(this, 1));
+    $parent.append($elem);
+
+    $elem = $("<div></div>");
+    $elem.attr("class", "indicator");
+    $parent.append($elem);
+
+    for (; i < l; i++) {
+        $img = $("<img>");
+        $span = $("<span></span>");
+        $span.on("click", this.selectSlide.bind(this));
+        if ((i + 1) === this.selectedIndex) {
+            $img.attr('class', 'active');
+            $span.attr('class', 'white');
+        }
+        $img.attr('src', imageArray[i]);
+        $span.attr('data-id', i + 1);
+        $parent.append($img);
+        $elem.append($span);
+    }
+
+    element.append($parent);
     this.startSlide();
 }
 
@@ -52,13 +89,12 @@ Slider.prototype.startSlide = function () {
     if (this.interval) {
         return;
     }
-    this.interval = setInterval(this.slide.bind(this, 1), this.delay);
+    this.interval = setInterval(this.slide.bind(this, 1), this.slideDelay);
 }
 
 Slider.prototype.stopSlide = function (e) {
     if (e.target.tagName === 'IMG') return;
     if (!this.interval) {
-        //this.interval = setInterval(this.slide.bind(this, 1), this.delay);
         return;
     }
     clearInterval(this.interval);
@@ -66,7 +102,7 @@ Slider.prototype.stopSlide = function (e) {
     if (this.startInterval) {
         return;
     }
-    this.startInterval = setInterval(this.startSlide.bind(this), 5000);
+    this.startInterval = setInterval(this.startSlide.bind(this), this.startDelay);
 }
 
 Slider.prototype.selectSlide = function (e) {
@@ -76,12 +112,9 @@ Slider.prototype.selectSlide = function (e) {
     this.slide(0);
 }
 
-var elems = document.getElementsByClassName("slider");
+var data, elems = document.getElementsByClassName("slider");
 for (var i = 0, len = elems.length; i < len; i++) {
-    // var config = {
-    //     delay: 2000,
-    //     imgs: ['win.jpg']
-    // };
-    new Slider(elems[i], 2000);
+    data = $(elems[i]).data('config');
+    new Slider(elems[i], data);
 }
 
